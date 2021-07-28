ba808e52882d9e152a2e6b947df4ce070b1b7c6c
Arnaldo Filho <arnalldofilho@gmail.com>
2021-07-28 16:09:48 -0300

    Delegando construtor, Herança Múltipla, Reescrevendo Métodos da superclasse.

---
+++ src/ObjetosOrientacao/ObjetosOrientacao/Autor.java
@@ -0,0 +1,15 @@
+package ObjetosOrientacao.ObjetosOrientacao;
+
+public class Autor {
+
+    String nome;
+    String email;
+    String cpf;
+   
+    void mostrarDetalhes() {
+
+        System.out.println("Detalhes do autor");
+        System.out.println("Nome: " + nome);
+        System.out.println("Email: " + email);
+    }
+}
